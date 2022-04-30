037afbdb27c5ae516e53b552701d736b182e1c78
Roman Filchenkov <rfmghz@gmail.com>
2022-05-01 01:55:26 +0300

    Time: 28 ms (97.00%), Space: 13.8 MB (65.68%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -2,7 +2,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         res = int(str(abs(x))[::-1])
         
-        if 2**31 - 1 > res > -2**31:
+        if res < 2**31 - 1:
             if x < 0:
                 return -res
             else:
