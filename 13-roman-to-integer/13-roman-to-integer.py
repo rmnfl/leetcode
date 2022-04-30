8374bf2a102cd1b0439f8ec50170c5b118748b37
Roman Filchenkov <rfmghz@gmail.com>
2022-05-01 01:42:55 +0300

    Time: 40 ms (97.89%), Space: 14 MB (32.00%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:       
+        digits = {
+            "I": 1,
+            "V": 5,
+            "X": 10, 
+            "L": 50, 
+            "C": 100, 
+            "D": 500, 
+            "M": 1000
+        }
+        
+        result = 0
+        previous = None
+        
+        for char in s[::-1]:
+            if previous and digits[char] < digits[previous]:
+                result -= digits[char]
+            else:
+                result += digits[char]
+            
+            previous = char
+            
+        return result
