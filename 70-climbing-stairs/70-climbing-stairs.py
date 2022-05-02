9f504f52413dca51a3138a2c087b95242b9bdab2
Roman Filchenkov <rfmghz@gmail.com>
2022-05-02 12:29:24 +0300

    Time: 36 ms (66.44%), Space: 13.9 MB (59.64%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    stairs_dic = {
+        1: 1,
+        2: 2
+    }
+    
+    def climbStairs(self, n: int) -> int:
+        if n in self.stairs_dic:
+            return self.stairs_dic[n]
+        else:
+            res = self.climbStairs(n-1) + self.climbStairs(n-2)
+            self.stairs_dic[n] = res
+            return res
