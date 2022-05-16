4bfda032cfaf6eacf9172bda4d7ebac1752df7e1
Roman Filchenkov <rfmghz@gmail.com>
2022-05-16 16:16:33 +0300

    Time: 53 ms (54.13%), Space: 13.8 MB (95.46%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -7,6 +7,6 @@ class Solution:
             for j in range(i):
                 if (dp[j] and s[j:i] in wordDict):
                     dp[i] = True
-                    break
                     
         return dp[-1]
+    
