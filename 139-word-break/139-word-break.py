e682130ff09f958d181522cf4e81575d0f9ba3a9
Roman Filchenkov <rfmghz@gmail.com>
2022-05-16 16:21:18 +0300

    Time: 72 ms (21.39%), Space: 13.9 MB (72.13%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,9 +2,13 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [0] * (len(s) + 1)
         dp[0] = True
+        max_len = max(map(len, wordDict))
+        print(max_len)
         
         for i in range(len(s) + 1):
             for j in range(i):
+                if i-j > max_len:
+                    continue
                 if (dp[j] and s[j:i] in wordDict):
                     dp[i] = True
                     
