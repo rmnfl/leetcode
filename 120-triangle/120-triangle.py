d787a7f9b94c9780e950dcfaaed1fd143463f8d2
Roman Filchenkov <rfmghz@gmail.com>
2022-05-17 19:15:32 +0300

    Time: 183 ms (5.11%), Space: 14.9 MB (62.02%) - LeetHub

---
+++ 120-triangle/120-triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        
+        for i in range(1, len(triangle)):
+            prev = list(zip([float('inf')] + triangle[i-1], triangle[i-1] + [float('inf')]))
+            print(prev)
+            
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(prev[j])
+                
+        return min(triangle[-1])
