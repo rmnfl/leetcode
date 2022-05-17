394a71de4b343817418fca29c890155d77a01e3f
Roman Filchenkov <rfmghz@gmail.com>
2022-05-17 19:16:00 +0300

    Time: 81 ms (66.21%), Space: 14.9 MB (92.85%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -3,9 +3,9 @@ class Solution:
         
         for i in range(1, len(triangle)):
             prev = list(zip([float('inf')] + triangle[i-1], triangle[i-1] + [float('inf')]))
-            print(prev)
             
             for j in range(len(triangle[i])):
                 triangle[i][j] += min(prev[j])
                 
         return min(triangle[-1])
+    
