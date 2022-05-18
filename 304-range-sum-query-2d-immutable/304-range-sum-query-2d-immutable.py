80f88dc8b9a1ebce486f577126d8fe99edf54000
Roman Filchenkov <rfmghz@gmail.com>
2022-05-18 19:28:49 +0300

    Time: 1891 ms (64.96%), Space: 24.8 MB (80.50%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -8,10 +8,10 @@ class NumMatrix:
         
         for i in range(m):
             for j in range(n):
-                dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] - dp[i][j] + matrix[i][j]
-                
+                dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] - dp[i][j] + matrix[i][j]        
         self.dp = dp
 
+    
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         return self.dp[row2+1][col2+1] - self.dp[row2+1][col1] - self.dp[row1][col2+1] + self.dp[row1][col1]
     
