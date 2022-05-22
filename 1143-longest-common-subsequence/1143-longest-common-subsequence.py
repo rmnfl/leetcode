23294da15416790b76edb94a70a80c07d28b74f2
Roman Filchenkov <rfmghz@gmail.com>
2022-05-22 21:02:15 +0300

    Time: 879 ms (26.68%), Space: 22.7 MB (57.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,7 +5,6 @@ class Solution:
         for i in range(len(text1)):
             for j in range(len(text2)):
                 if text1[i] == text2[j]:
-                    print(i, j, dp[i-1][j-1])
                     dp[i+1][j+1] = dp[i][j] + 1
                 else:
                     dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
