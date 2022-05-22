4e7342d273a85c3146ed69efd538b3219a009b9f
Roman Filchenkov <rfmghz@gmail.com>
2022-05-22 19:32:13 +0300

    Time: 6266 ms (11.93%), Space: 22.7 MB (5.68%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(s)):
             dp[i][i] = True
                 
-        for i in range(len(s), -1, -1):
+        for i in range(len(s)-1, -1, -1):
             for j in range(i+1, len(s)):
                 if s[i]==s[j]:
                     if j-i==1 or dp[i+1][j-1]:
