5ed7152bcf8a3649019dbffd2a2aec56d1745323
Roman Filchenkov <rfmghz@gmail.com>
2022-06-23 20:03:34 +0300

    Time: 66 ms (47.09%), Space: 13.9 MB (77.81%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,3 @@
 class Solution(object):
-    def isAnagram(self, s, t):
-        dict_s = Counter(s)
-        dict_t = Counter(t)
-        
-        return dict_s == dict_t
+    def isAnagram(self, s, t):       
+        return Counter(s) == Counter(t)
