88614a49b008edfb2660197a895edefd7a659797
Roman Filchenkov <rfmghz@gmail.com>
2022-06-24 16:36:23 +0300

    Time: 163 ms (23.10%), Space: 16.8 MB (74.07%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,3 +1,4 @@
 class Solution(object):
     def topKFrequent(self, nums, k):
-        return list(zip(*Counter(nums).most_common(k) ))[0]
+        return list(zip(*Counter(nums).most_common(k)))[0]
+    
