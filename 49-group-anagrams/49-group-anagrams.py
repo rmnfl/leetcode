c70574b2b1fbc85edca456708dba6fdb1c351ff6
Roman Filchenkov <rfmghz@gmail.com>
2022-06-24 15:57:41 +0300

    Time: 170 ms (21.21%), Space: 19.6 MB (26.37%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        anagrams = defaultdict(list)
+        
+        for s in strs:
+            counter = [0] * 26
+            
+            for c in s:
+                counter[ord(c) - ord("a")] += 1
+                
+            anagrams[tuple(counter)].append(s)
+    
+        return anagrams.values()
+        
