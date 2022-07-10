b321c611f7faee498738f88eb4120affcc23ff44
Roman Filchenkov <rfmghz@gmail.com>
2022-07-10 23:43:59 +0300

    Time: 120 ms (44.76%), Space: 13.6 MB (19.58%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        return (self.isSquaresValid(board) and 
+                self.isHorizontalLinesValid(board) and 
+                self.isVerticalLinesValid(board))
+            
+    
+    def isHorizontalLinesValid(self, board):
+        for i in board:
+            line = [n for n in i if n != '.']
+            if len(line) != len(set(line)):
+                return False
+        return True
+    
+    
+    def isVerticalLinesValid(self, board):
+        for i in zip(*board):
+            line = [n for n in i if n != '.']
+            if len(line) != len(set(line)):
+                return False
+        return True
+    
+    def isSquaresValid(self, board):
+        for i in range(0, 7, 3):
+            for j in range(0, 7, 3):
+                square = [board[m][n] for m in range(i, i+3) for n in range(j, j+3) if board[m][n] != '.']
+                if len(square) != len(set(square)):
+                    return False
+        return True
