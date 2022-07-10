16f9412a174e2445174b4f013b163e12973dd8ee
Roman Filchenkov <rfmghz@gmail.com>
2022-07-11 00:22:28 +0300

    Time: 277 ms (93.02%), Space: 30.6 MB (10.45%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -6,12 +6,10 @@ class Solution(object):
         
         for n in nums:
             if n-1 not in nums:
-                curr_num = n
                 curr_len = 1
                 
-                while curr_num+1 in nums:
+                while n+curr_len in nums:
                     curr_len += 1
-                    curr_num += 1
                 
                 max_len = max(max_len, curr_len)
             
