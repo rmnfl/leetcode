fa4474bdd03b4180d851f89453a4dcf66cde1f3e
Roman Filchenkov <rfmghz@gmail.com>
2022-07-11 00:32:10 +0300

    Time: 644 ms (46.66%), Space: 29.1 MB (18.93%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,40 +1,16 @@
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.parent = self
-        self.size = 1
-    
-class UnionFind:
+class Solution(object):
+    def longestConsecutive(self, nums):
+        nums = set(nums)
         
-    def find(self, node):
-        if node.parent != node:
-            node.parent = self.find(node.parent)
-        return node.parent
-    
-    def union(self, node1, node2):
-        parent_1 = self.find(node1)
-        parent_2 = self.find(node2)
-        if parent_1 != parent_2:
-            parent_2.parent = parent_1
-            parent_1.size += parent_2.size
-        return parent_1.size
+        max_len = 0
+        
+        for n in nums:
+            if n-1 not in nums:
+                curr_len = 1
                 
-        
-        
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        uf = UnionFind()
-        nodes = {}
-        max_size = 0
-        for num in nums:
-            if num not in nodes:
-                node = Node(num)
-                nodes[num] = node
-                size = 1
-                if num + 1 in nodes:
-                    size = uf.union(node, nodes[num+1])
-                if num - 1 in nodes:
-                    size = uf.union(node, nodes[num-1])
-                max_size = max(max_size, size)
+                while n+curr_len in nums:
+                    curr_len += 1
                 
-        return max_size
+                max_len = max(max_len, curr_len)
+            
+        return max_len
