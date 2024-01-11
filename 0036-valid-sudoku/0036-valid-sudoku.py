85f52a8a47997c21458a5e47b63c708cfd69ef64
Roman Filchenkov <rfmghz@gmail.com>
2024-01-11 14:08:11 +0300

    Time: 90 ms (89.77%), Space: 17.2 MB (33.56%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,26 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        def areRowsValid():
-            for row in board:
-                nums = list(filter(str.isdigit, row))
-                if len(nums) != len(set(nums)):
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if board[r][c] in rows[r]:
                     return False
-            return True
-
-        def areColumnsValid():
-            for column in zip(*board):
-                nums = list(filter(str.isdigit, column))
-                if len(nums) != len(set(nums)):
+                if board[r][c] in cols[c]:
                     return False
-            return True
+                if board[r][c] in squares[(r//3, c//3)]:
+                    return False
+                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
 
-        def areSquaresValid():
-            for i in range(0, 7, 3):
-                for j in range(0, 7, 3):
-                    square = [board[n][m] for n in range(i, i+3) for m in range(j, j+3)]
-                    nums = list(filter(str.isdigit, square))
-                    if len(nums) != len(set(nums)):
-                        return False
-            return True
-
-        return areRowsValid() and areColumnsValid() and areSquaresValid()
+        return True
