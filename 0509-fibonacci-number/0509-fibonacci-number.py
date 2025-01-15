c2bcf75e51a062d5b83f19e1bb376ed7f7fa9413
Roman Filchenkov <rfmghz@gmail.com>
2025-01-15 22:01:25 +0300

    Time: 36 ms (69.81%), Space: 17.5 MB (48.56%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
+        cache = dict()
+        cache[0] = 0
+        cache[1] = 1
 
-        return self.fib(n-1) + self.fib(n-2)
+        def rec(n):
+            nonlocal cache
+            if n in cache:
+                value = cache[n]
+            else:
+                value = rec(n-1) + rec(n-2)
+                cache[n] = value
+            return value
+        
+        return rec(n)
